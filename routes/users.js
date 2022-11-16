9cf9a2ef5ade4beb5910de0a7d2941d491209780
stemax <max01oe@yandex.ru>
2022-11-16 10:56:45 +0300

    Docker settings and modern variables and settings to 3001 port

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const users = [{id:1, name:'Test user 1'}, {id:2, name:'Test user 2'},]
+  res.send(users);
 });
 
 module.exports = router;
